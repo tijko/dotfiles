c9062e82bc2d705d725d2910e0757b1038c08204
Tim Konick <konick781@gmail.com>
2024-04-22 22:56:00 -0400

    Nerd Font

--- dwm-config.def.h
+++ dwm-config.def.h
@@ -7,8 +7,9 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 #define ICONSIZE 16   /* icon size */
 #define ICONSPACING 5 /* space between icon and title */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "Font Awesome 6 Brands Regular:size=11",
+                                        "JetBrainsMonoNL Nerd Font Propo:size=11};
+static const char dmenufont[]       = "Inconsolata Nerd Font Propo:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -21,7 +22,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4"};
 
 static const Rule rules[] = {
 	/* xprop(1):
