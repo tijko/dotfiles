7b6a62c837526c62227aed8d137e4cf4a1cd0234
Tim Konick <konick781@gmail.com>
2026-04-19 20:17:22 -0400

    Use a sub-class Exception

--- remove-cmd.py
+++ remove-cmd.py
@@ -7,19 +7,25 @@ import sys
 from pathlib import Path
 
 
+class RemovalError(Exception):
+
+    def __init__(self, errno, strerror):
+        super().__init__(errno, strerror)
+        self.errno = errno
+        self.strerror = strerror
+
+
 def sanitize_input(raw_input):
-    if not isinstance(raw_input, list):
-        print('Error input arguments')
-        sys.exit(0)
-    args = raw_input[1]
-    if 'f' in args:
-        args = args.replace('f', '')
-    cwd = os.getcwd()
-    filepath = Path(raw_input[2])
-    if not filepath.exists():
-        print('Error input arguments')
-        sys.exit(0)
-    remove_cmd(filepath, args)
+    try:
+        args = raw_input[1]
+        if 'f' in args:
+            args = args.replace('f', '')
+        cwd = os.getcwd()
+        filepath = Path(raw_input[1])
+        remove_cmd(filepath, args)
+    except:
+        raise RemovalError("Invalid Input", -1)
+    
 
 def remove_cmd(fp, args):
     ps_cmd = 'rm --interactive=always --verbose' + ' ' + args + ' ' + str(fp.absolute())
@@ -27,7 +33,4 @@ def remove_cmd(fp, args):
 
 if __name__ == '__main__':
     args = sys.argv
-    if len(args) < 3:
-        print('Error input arguments')
-        sys.exit(0)
     sanitize_input(args)
